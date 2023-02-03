7e6e8188aee2b1196071018ab7878d7f8c86d68c
upendrabandi2023 <upendrabandi2023@gmail.com>
2023-02-04 01:03:40 +0530

    first release done

---
+++ src/main/java/com/project/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,28 @@
+package com.project.ecommerce.config;
+
+import com.project.ecommerce.entity.Product;
+import com.project.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+        HttpMethod[] theUnSupportedActions={HttpMethod.PUT,HttpMethod.POST,HttpMethod.DELETE};
+        //disable http methods for put post delete
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnSupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnSupportedActions)));
+
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnSupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnSupportedActions)));
+    }
+}
